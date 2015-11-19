e9c12a4ce67bd67c2db44e3c71ad741b64918b18
snowc4636 <snowc4636@HRH-IC0027318.wrdsb.ca>
2015-11-19 12:19:01 -0500

    Assignment 5 complete

--- src/A5Q1.java
+++ src/A5Q1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author snowc4636
@@ -16,70 +15,65 @@ public class A5Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // 
-Scanner input = new Scanner(System.in);
+        //
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Please enter a word");
-        String word = input.nextLine();
+        String definition = input.nextLine();
 
 
 
-        String Matten = word;
-//Put ee at the end if it starts with a vowel
+        String Englisch = definition;
+//If is starts with a vowel replace the first vowel with an i
 
-        if (Matten.startsWith("a")
-                || Matten.startsWith("o")
-                || Matten.startsWith("e")
-                || Matten.startsWith("u")) {
-            Matten = Matten.substring(1);
-            Matten = "i" + Matten;
+        if (Englisch.startsWith("a")
+                || Englisch.startsWith("o")
+                || Englisch.startsWith("e")
+                || Englisch.startsWith("u")) {
+            String number1 = Englisch.substring(0, 1);
+            Englisch = Englisch.replaceFirst(number1, "i");
+//If englisch ends with a vowel add "hee" to the end
+                        if (Englisch.endsWith("a")
+                                || Englisch.endsWith("i")
+                                || Englisch.endsWith("e")
+                                || Englisch.endsWith("o")
+                                || Englisch.endsWith("u")) {
+                            System.out.println(" In Englisch: " + Englisch + "hee");
+//add "ee" top the end                          
+                        }else{
+                             System.out.println(" In Englisch: " + Englisch + "ee");
+                          
+                        }
         } else {
 
-            while (!word.startsWith("a")
-                    && !word.startsWith("e")
-                    && !word.startsWith("i")
-                    && !word.startsWith("o")
-                    && !word.startsWith("u")) {
+            while (!definition.startsWith("a")
+                    || !definition.startsWith("e")
+                    || !definition.startsWith("i")
+                    || !definition.startsWith("o")
+                    || !definition.startsWith("u")) {
 
-                if (word.equals("END")) {
-                    System.out.println("No words? ");
+                if (definition.equals("Done")) {
+                    System.out.println("Nothing found ");
                     break;
                 }
-//if word starts with a vowel
-                if (!Matten.startsWith("a")
-                        && !Matten.startsWith("e")
-                        && !Matten.startsWith("o")
-                        && !Matten.startsWith("u")
-                        && !Matten.startsWith("i")
-                        && !Matten.startsWith("y")) {
-                    String one = Matten.substring(0, 1);
-                    String second = Matten.substring(1);
-                    Matten = second + one;
-
-                    if (Matten.startsWith("o")
-                            || Matten.startsWith("a")
-                            || Matten.startsWith("e")
-                            || Matten.startsWith("u")) {
-                        String letter1 = Matten.substring(0, 1);
-                        Matten = Matten.replaceFirst(letter1, "i");
-                        System.out.println(" In Matten: " + Matten + "ee");
-                        break;
-                    }
-                } else {
-                    String letter1 = Matten.substring(0, 1);
-                    Matten = Matten.replaceFirst(letter1, "i");
-                    System.out.println("Matten: " + Matten + "ee");
-                    break;
+//This will happen if the word starts with a vowel
+                if (!Englisch.startsWith("e")
+                        && !Englisch.startsWith("o")
+                        && !Englisch.startsWith("u")
+                        && !Englisch.startsWith("i")
+                        && !Englisch.startsWith("y")
+                        && !Englisch.startsWith("a")) {
+                    String one = Englisch.substring(0, 1);
+                    String second = Englisch.substring(1);
+                    Englisch = second + one;
+                    String number1 = Englisch.substring(0, 1);
+                    Englisch = Englisch.replaceFirst(number1, "i");
+                    System.out.println(" In Englisch: " + Englisch + "ee");
+            
                 }
-
+                
+                
             }
         }
     }
 }
-
-            
-        
-                
-
-    
-
